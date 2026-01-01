0ca568f68b90934531af6d0a64f2d1eae8fd8d23
wndgur2 <wndgur76@naver.com>
2026-01-02 04:16:32 +0900

    feat: api 기본 파일 생성

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -19,7 +19,7 @@ export default function Input({
       value={value}
       placeholder={placeholder}
       onChange={onChange}
-      className={`px-4 py-3 md:px-6 md:py-4 text-base md:text-lg bg-white/25 rounded-xl ${className}`}
+      className={`px-4 py-3 md:px-6 md:py-4 text-base md:text-lg bg-white/75 rounded-xl ${className}`}
     />
   )
 }
